a02bc4db90c018f2eb6d9b9fb23466721f76b302
Miroslav Šollar <52739918+mirasollar@users.noreply.github.com>
2022-12-24 11:09:50 +0100

    Add files via upload

---
+++ src/mso_hapi.py
@@ -0,0 +1,13 @@
+import requests
+
+class HubspotAPI:
+    def __init__(self, token):
+        self.apiurl = 'https://api.hubapi.com/crm/v3/objects'
+        self.headers = {'Accepts': 'application/json', 'authorization': "Bearer " + token}
+        
+    def getDeals(self, limit=5):
+        url = self.apiurl + '/deals'
+        parameters = {'limit': limit}
+        r = requests.get(url, headers=self.headers, params=parameters)
+        json_data = r.json()['results']
+        return json_data
