32233b3e5e358c3eddddbf744d300f5e1f7caa7a
Miroslav Šollar <52739918+mirasollar@users.noreply.github.com>
2022-12-24 12:00:07 +0100

    Update mso_hapi.py

--- src/mso_hapi.py
+++ src/mso_hapi.py
@@ -1,10 +1,11 @@
 import requests
 
+
 class HubspotAPI:
     def __init__(self, token):
         self.apiurl = 'https://api.hubapi.com/crm/v3/objects'
         self.headers = {'Accepts': 'application/json', 'authorization': "Bearer " + token}
-        
+
     def getDeals(self, limit=5):
         url = self.apiurl + '/deals'
         parameters = {'limit': limit}
